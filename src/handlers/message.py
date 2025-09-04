8727f6acae94b823f3e7d516131e6a8d401a5677
Yaroslav Morozov <cursay@Yaroslavs-MacBook-Air.local>
2025-09-04 10:58:16 +0500

    minor update

--- src/handlers/message.py
+++ src/handlers/message.py
@@ -25,17 +25,19 @@ router.message.filter(F.chat.type.in_({ChatType.PRIVATE}),)
 
 @router.message(Command(commands=["start"]))
 async def start(message: Message) -> None:
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Куда идти?")]+
-            [InlineKeyboardButton(text="Достижения")]+
-            [InlineKeyboardButton(text="Настройки")]
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Куда идти?", callback_data="go"),
+            InlineKeyboardButton(text="Достижения", callback_data="achievements"),
+            InlineKeyboardButton(text="Настройки", callback_data="settings")
         ]
-    )
+    ])
+    
     await message.answer(
         (
             f"👋 <b>Привет, дорогой Друг!</b> 👋\n\n"
             f"Я помогу тебе сориентироваться в новом кампусе ИРИТ-РтФ УрФУ в Новокольцовском.\n"
             f"Пожалуйста, выбери действие: "
-        )
+        ), reply_markup=keyboard
     )
